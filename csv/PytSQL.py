33946dcac17f7d92dd7be3ecac5ce348727cb8c7
Astraalto <cyberindigos@gmail.com>
2026-03-11 22:52:30 +0200

    SQL check begin

---
+++ csv/PytSQL.py
@@ -0,0 +1,16 @@
+import sqlite3
+
+conn = sqlite3.connect("myynti.db")
+cursor = conn.cursor()
+
+cursor.execute("""
+               CREATE TABLE IF NOT EXIST myynti (
+               asiakas TEXT,
+               määrä INTEGER
+               )
+               """)
+
+cursor.exexute("INSERT INTO myynti VALUES ('Sanna', 130)")
+cursor.exexute("INSERT INTO myynti VALUES ('Jukka', 90)")
+
+conn.comit()
